975e8593ea370932c550f625d566ee07cdea8273
21173447 <tshegofatsononyane009@gmail.com>
2024-11-21 15:14:07 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req,res) => {
+  res.send("YOUT ALL GOOD");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+  try {
+    await connectDB();
+    console.log(`Server started at http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Failed to connect to DB", error);
+    process.exit(1);
+  }
+});
