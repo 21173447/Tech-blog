b68b23a2a06d06c1b1279b5e80af41441cfed699
21173447 <tshegofatsononyane009@gmail.com>
2024-11-25 14:39:57 +0200

    navbar

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div>
+      <ul>
+        <li>LOGO</li>
+        <li>CREATE ACCOUNT </li>
+        <li>LOGIN</li>
+        <li>WRITE</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
