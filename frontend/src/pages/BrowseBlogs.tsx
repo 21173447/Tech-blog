a7eec7146c234537cca475d813578c7f81b171b6
21173447 <tshegofatsononyane009@gmail.com>
2024-11-30 14:57:23 +0200

    creating  subscribe function

--- frontend/src/pages/BrowseBlogs.tsx
+++ frontend/src/pages/BrowseBlogs.tsx
@@ -26,47 +26,46 @@ const BrowseBlogs = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-10 w-[50%] place-content-center justify-items-center mx-auto">
-  {blogs && blogs.length > 0 ? (
-    blogs.map((blog) => (
-      <div
-        key={blog._id}
-        className=" w-full overflow-hidden transform hover:scale-105 hover:translate-x-1 hover:translate-y-1 transition-all duration-200"
-        onClick={() => handleClick(blog._id)}
-      >
-        <img
-          src={blog.image}
-          alt="Card Image"
-          className="w-full h-32 object-cover" 
-        />
-        <div className="text-sm p-2"> 
-          <h3 className="font-semibold text-lg text-black mb-1">
-            {blog.title.substring(0, 27)}
-          </h3>
-          <p className="text-black mb-2 text-xs">
-            {blog.content.substring(0, 80)}....
-          </p>
-          <div className="text-xs text-black space-y-1">
-            <p>
-              <span className="font-semibold"></span> {blog.name}
-            </p>
-            <p>
-              <span className="font-semibold">Posted:</span>{" "}
-              {blog.createdAt
-                ? new Date(blog.createdAt).toLocaleDateString()
-                : "N/A"}
-            </p>
-          </div>
-        </div>
+        {blogs && blogs.length > 0 ? (
+          blogs.map((blog) => (
+            <div
+              key={blog._id}
+              className=" w-full overflow-hidden transform hover:scale-105 hover:translate-x-1 hover:translate-y-1 transition-all duration-200"
+              onClick={() => handleClick(blog._id)}
+            >
+              <img
+                src={blog.image}
+                alt="Card Image"
+                className="w-full h-32 object-cover"
+              />
+              <div className="text-sm p-2">
+                <h3 className="font-semibold text-lg text-black mb-1">
+                  {blog.title.substring(0, 27)}
+                </h3>
+                <p className="text-black mb-2 text-xs">
+                  {blog.content.substring(0, 80)}....
+                </p>
+                <div className="text-xs text-black space-y-1">
+                  <p>
+                    <span className="font-semibold"></span> {blog.name}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Posted:</span>{" "}
+                    {blog.createdAt
+                      ? new Date(blog.createdAt).toLocaleDateString()
+                      : "N/A"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-white text-center">No blogs available.</p>
+        )}
       </div>
-    ))
-  ) : (
-    <p className="text-white text-center">No blogs available.</p>
-  )}
-</div>
-<div className="flex justify-center pt-5">
+      <div className="flex justify-center pt-5">
         <hr className="border-[3px] w-[60%] border-black" />
       </div>
-
     </div>
   );
 };
