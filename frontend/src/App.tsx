3c44229774e4f86f88f5cf50526cd4ccd44e38f8
21173447 <tshegofatsononyane009@gmail.com>
2024-11-30 17:50:09 +0200

    last ui fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom'; 
-import Nav from './Components/Nav'; 
-import Subscribe from './Components/Subscribe';
-import FeaturedBlogs from './Components/FeaturedBlogs';
-import Footer from './Components/Footer';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Subscribe from "./Components/Subscribe";
+import FeaturedBlogs from "./Components/FeaturedBlogs";
+import Footer from "./Components/Footer";
 
 const App: React.FC = () => {
   const location = useLocation();
 
-  
-  const isHomePage = location.pathname === '/';
-  const isLoginPage = location.pathname === '/login';
-  const isRegisterPage = location.pathname === '/register';
-  const  isCreateBlog = location.pathname === '/create';
+  const isHomePage = location.pathname === "/";
+  const isLoginPage = location.pathname === "/login";
+  const isRegisterPage = location.pathname === "/register";
+  const isCreateBlog = location.pathname === "/create";
 
-  
-  const showFeaturedAndSubscribe = !(isHomePage || isLoginPage || isRegisterPage || isCreateBlog);
+  const showFeaturedAndSubscribe = !(
+    isHomePage ||
+    isLoginPage ||
+    isRegisterPage ||
+    isCreateBlog
+  );
 
   return (
     <div>
